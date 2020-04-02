2eb9dc27a678440131c1d5a78c4baa9ba1aa6bd3
Albert Laiuste <albertlaiuste@gmail.com>
2020-04-02 16:02:08 +0300

    Draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,12 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Use legacy openGL to draw a triangle */
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
